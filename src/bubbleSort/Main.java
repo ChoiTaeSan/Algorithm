926d81ee1c3b2b855ebb308f4a2b5176020ed7ab
Choi TaeSan <xotks7524@gmail.com>
2023-12-22 00:29:37 +0900

    Feat: bubbleSort Build

--- src/bubbleSort/Main.java
+++ src/bubbleSort/Main.java
@@ -1,15 +1,15 @@
 package bubbleSort;
 
 public class Main {
-    public static void main(String args[]) {
-        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
         bubbleSort(arr);
         System.out.println("정렬된 배열: ");
         printArray(arr);
     }
 
     // 버블 정렬 메소드
-    public static void bubbleSort(int arr[]) {
+    public static void bubbleSort(int[] arr) {
         int n = arr.length;
         for (int i = 0; i < n-1; i++)
             for (int j = 0; j < n-i-1; j++)
@@ -22,9 +22,8 @@ public class Main {
     }
 
     // 배열을 출력하는 메소드
-    public static void printArray(int arr[]) {
-        for (int i=0; i<arr.length; i++)
-            System.out.print(arr[i] + " ");
+    public static void printArray(int[] arr) {
+        for (int j : arr) System.out.print(j + " ");
         System.out.println();
     }
 }
