3a2c1210d3a0b4b0b4f9970358748db44e83e01a
Choi TaeSan <xotks7524@gmail.com>
2023-12-24 18:59:12 +0900

    Feat: insertionSort Build

--- src/insertionSort/Main.java
+++ src/insertionSort/Main.java
@@ -25,8 +25,7 @@ public class Main {
 
     // 배열 출력 메소드
     static void printArray(int[] arr) {
-        for (int i = 0; i < arr.length; ++i)
-            System.out.print(arr[i] + " ");
+        for (int j : arr) System.out.print(j + " ");
 
         System.out.println();
     }
